baf15a81d9a7c7b28228b818b01fc2a869a800d6
tsahil01 <sahiltiwaskar.21@stvincentngp.edu.in>
2024-08-20 01:55:48 +0530

    chore: Update landing page banner color and wallet description

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,11 +2,11 @@ export default function Header() {
   return (
     <>
       <section className="flex flex-col items-start gap-2 px-4 py-8 md:py-12 md:pb-8 lg:py-12 lg:pb-10">
-        <h1 className="text-3xl font-bold leading-tight tracking-tighter md:text-6xl">
+        <h1 className="text-4xl font-bold leading-tight tracking-tighter md:text-7xl">
           Pouch
         </h1>
         <p className="max-w-2xl text-lg font-light text-foreground">
-          An implementation of real world web3 wallets.
+          An open-source HD wallet generator for Sol and Eth.
         </p>
       </section>
     </>
