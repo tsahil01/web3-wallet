bbb3170a77a7c7884f2e6fc9bd120e0aae846e48
Sahil Tiwaskar <118750218+tsahil01@users.noreply.github.com>
2024-08-21 04:06:27 +0000

    fix pad

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ import { useRecoilState } from "recoil";
 export default function Home() {
   const [seedPhase, setSeedPhase] = useRecoilState(seedPhaseAtom);
   return (
-    <div className="md:p-2">
+    <div className="md:p-2 px-4 overflow-auto">
       <div className="md:container flex flex-col gap-4">
         <div className="flex justify-between my-auto align-middle border-b-2 mx-4 md:mx-0">
           <Header />
