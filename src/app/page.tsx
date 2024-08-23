1334f4cc1f8c8dd53b75d68ba9f466a10a29034f
tsahil01 <sahiltiwaskar.21@stvincentngp.edu.in>
2024-08-24 00:11:40 +0530

    update theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,17 +5,23 @@ import { seedPhaseAtom } from "@/atom/seedPhaseAtom";
 import Header from "@/components/Header";
 import MainComponent from "@/components/Main";
 import SeedPhaseComponent from "@/components/SeedPhase";
+import { ThemeToggle } from "@/components/ThemeSwitch";
+import { useTheme } from "next-themes";
 import { useRecoilState } from "recoil";
 
 export default function Home() {
   const [seedPhase, setSeedPhase] = useRecoilState(seedPhaseAtom);
+  const { setTheme } = useTheme()
+
   return (
     <div className="md:p-2 px-4 overflow-auto">
       <div className="md:container flex flex-col gap-4">
         <div className="flex justify-between my-auto align-middle border-b-2 mx-4 md:mx-0">
           <Header />
-
+          <div className="flex gap-2 my-auto">
           {seedPhase.length > 0 && <SeedPhaseComponent seedPhase={seedPhase} />}
+          <ThemeToggle />
+          </div>
         </div>
         <div className="mt-9 h-full overflow-auto">
           <MainComponent />
--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,11 +2,14 @@
 
 import React from "react";
 import { RecoilRoot } from "recoil";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 export default function Providers({ children }: React.PropsWithChildren<{}>) {
   return (
     <>
-      <RecoilRoot>{children}</RecoilRoot>
+      <NextThemesProvider attribute="class" defaultTheme="system">
+        <RecoilRoot>{children}</RecoilRoot>
+      </NextThemesProvider>
     </>
   );
 }
