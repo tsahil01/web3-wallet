601ce4bafa9fef868afca33b9c219867f6499556
tsahil01 <sahiltiwaskar.21@stvincentngp.edu.in>
2024-09-11 23:07:21 +0530

    airDrop

--- src/app/airdrop/page.tsx
+++ src/app/airdrop/page.tsx
@@ -2,16 +2,62 @@
 
 import AirdropMainComponent from "@/components/AirDropMain";
 import Header from "@/components/Header";
-
+import { Button } from "@/components/ui/button";
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import {
+  WalletDisconnectButton,
+  WalletMultiButton,
+} from "@solana/wallet-adapter-react-ui";
+import { LAMPORTS_PER_SOL } from "@solana/web3.js";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const wallet = useWallet();
+  const { connection } = useConnection();
+  const [balance, setBalance] = useState<number | string | null>(null);
+
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
+  async function getBalance() {
+    if (wallet.publicKey) {
+      const balance = await connection.getBalance(wallet.publicKey);
+      setBalance(balance / LAMPORTS_PER_SOL);
+    }
+  }
+
   return (
     <div className="md:p-2 px-4 overflow-auto">
       <Header />
       <div className="mt-5 h-full overflow-auto md:container ">
+        <div className="flex md:flex-row flex-col justify-between mb-5 gap-5">
+          <div className="flex flex-row gap-4 my-auto mx-auto md:mx-0">
+            <WalletMultiButton />
+            <WalletDisconnectButton />
+          </div>{" "}
+          {wallet.publicKey && (
+            <div className="flex flex-col gap-1 mx-auto md:mx-0">
+              <h1 className="text-5xl font-bold my-auto">{balance} SOL</h1>
+              <Button
+                variant={"link"}
+                size={"sm"}
+                onClick={getBalance}
+                className="my-auto"
+              >
+                Get Balance
+              </Button>
+            </div>
+          )}
+        </div>
         <AirdropMainComponent />
       </div>
     </div>
   );
 }
-    
--- src/components/AirDropMain.tsx
+++ src/components/AirDropMain.tsx
@@ -1,118 +1,79 @@
-
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import { useState } from "react";
 import { Input } from "@/components/ui/input";
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import { LAMPORTS_PER_SOL } from "@solana/web3.js";
+import { toast } from "./ui/use-toast";
+import { Coins } from "lucide-react";
 
 export default function AirdropMainComponent() {
-  const [activeTab, setActiveTab] = useState("userTokens");
-  const [solanaAddress, setSolanaAddress] = useState("");
-  const [tokenName, setTokenName] = useState("");
-  const [tokenSymbol, setTokenSymbol] = useState("");
-  const [tokenSupply, setTokenSupply] = useState("");
-  const [tokenDetailsAddress, setTokenDetailsAddress] = useState("");
+  const [activeTab, setActiveTab] = useState("airDrop");
+  const [sol, setSol] = useState<string | number>();
+
+  const wallet = useWallet();
+  const { connection } = useConnection();
+
+  async function sendAirdropToUser() {
+    if (wallet.publicKey) {
+      const sendSol = await connection.requestAirdrop(
+        wallet.publicKey,
+        Number(sol) * LAMPORTS_PER_SOL
+      );
+      if (sendSol) {
+        toast({
+          title: "Airdrop Success",
+          description: `Airdrop of ${sol} SOL was successful`,
+        });
+      }
+    } else {
+      console.error("Wallet public key is null");
+    }
+  }
 
   return (
     <div className="flex flex-col md:gap-7 gap-4 mb-8 justify-normal">
       <div className="flex gap-2 rounded-lg mx-auto">
         <Button
           variant="link"
-          onClick={() => setActiveTab("createToken")}
-          className={`flex gap-1 ${activeTab === "createToken" ? "underline" : ""}`}
+          onClick={() => setActiveTab("airDrop")}
+          className={`flex gap-1 ${activeTab === "airDrop" ? "underline" : ""}`}
         >
-          Create Token
+          AirDrop SOL
         </Button>
         <Separator orientation="vertical" className="h-8 my-auto" />
-        <Button
-          variant="link"
-          onClick={() => setActiveTab("userTokens")}
-          className={`flex gap-1 ${activeTab === "userTokens" ? "underline" : ""}`}
-        >
-          Your Tokens
-        </Button>
-        <Separator orientation="vertical" className="h-8 my-auto" />
-        <Button
-          variant="link"
-          onClick={() => setActiveTab("tokenDetails")}
-          className={`flex gap-1 ${activeTab === "tokenDetails" ? "underline" : ""}`}
-        >
-          Token Details
-        </Button>
       </div>
 
-      {activeTab === "createToken" && (
+      {activeTab === "airDrop" && (
         <div className="flex flex-col gap-7 px-4">
           <div className="flex flex-col gap-1">
-            <h1 className="md:text-3xl text-2xl font-bold">Create a New Token</h1>
+            <h1 className="md:text-3xl text-2xl font-bold">AirDrop Solana</h1>
             <p className="text-xs dark:text-slate-500 text-gray-400">
-              Fill in the details below to create your Solana token.
+              Get free SOL Airdroped to your wallet.
             </p>
           </div>
-          <div className="md:grid md:grid-cols-8 flex flex-col gap-4 mt-3">
-            <Input
-              className="md:col-span-3"
-              type="text"
-              placeholder="Token Name"
-              value={tokenName}
-              onChange={(e) => setTokenName(e.target.value)}
-            />
-            <Input
-              className="md:col-span-2"
-              type="text"
-              placeholder="Token Symbol"
-              value={tokenSymbol}
-              onChange={(e) => setTokenSymbol(e.target.value)}
-            />
-            <Input
-              className="md:col-span-2"
-              type="number"
-              placeholder="Supply"
-              value={tokenSupply}
-              onChange={(e) => setTokenSupply(e.target.value)}
-            />
-            <Button className="md:col-span-1">Create Token</Button>
-          </div>
-        </div>
-      )}
-
-      {activeTab === "userTokens" && (
-        <div className="flex flex-col gap-7 px-4">
-          <div className="flex flex-col gap-1">
-            <h1 className="md:text-3xl text-2xl font-bold">View User&apos;s Solana Token</h1>
-            <p className="text-xs dark:text-slate-500 text-gray-400">
-              Enter the address to view token information.
-            </p>
-          </div>
-          <div className="md:grid md:grid-cols-8 flex flex-col gap-4 mt-3">
-            <Input
-              className="md:col-span-7"
-              type="text"
-              placeholder="Enter Solana address"
-              value={solanaAddress}
-              onChange={(e) => setSolanaAddress(e.target.value)}
-            />
-            <Button className="md:col-span-1">View Tokens</Button>
-          </div>
-        </div>
-      )}
-
-      {activeTab === "tokenDetails" && (
-        <div className="flex flex-col gap-7 px-4">
-          <div className="flex flex-col gap-1">
-            <h1 className="md:text-3xl text-2xl font-bold">Token Details</h1>
-            <p className="text-xs dark:text-slate-500 text-gray-400">
-              Enter the token address to view its details on Solana.
-            </p>
-          </div>
-          <div className="md:grid md:grid-cols-8 flex flex-col gap-4 mt-3">
-            <Input
-              className="md:col-span-6"
-              type="text"
-              placeholder="Enter Token Address"
-              value={tokenDetailsAddress}
-              onChange={(e) => setTokenDetailsAddress(e.target.value)}
-            />
-            <Button className="md:col-span-2">View Details</Button>
+          <div className="flex flex-row justify-between">
+            <div className="flex flex-col gap-3 mt-3 mx-auto border p-4 rounded">
+              <Input
+                className=""
+                type="text"
+                placeholder="Enter SOL Amount"
+                onChange={(e) => setSol(e.target.value)}
+              />
+              <Button
+                className="md:col-span-1 flex gap-1"
+                disabled={!wallet.publicKey}
+                onClick={sendAirdropToUser}
+              >
+                <Coins size={24} />
+                Get Airdrop
+              </Button>
+              {!wallet.publicKey && (
+                <p className="text-sm text-center text-muted-foreground">
+                  Connect wallet to get airdrop
+                </p>
+              )}
+            </div>
           </div>
         </div>
       )}
